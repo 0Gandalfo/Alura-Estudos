7a43aaeb67e7ce7888658f2127535f5399347f58
Luiz Henrique Garcia Ribeiro <luizhenriquegarciaribeiro@hotmail.com>
2024-03-06 15:26:18 -0300

    exercicios no Cmd
    organizando pastas e add novos exercicios concluidos

---
+++ CursoCMD/Criando Variaveis no cmd/script1.bat
@@ -0,0 +1,9 @@
+@echo off
+rem Limpando a tela
+cls
+set /p nome=Digite Seu nome=
+set /p email=Digite seu e-mail=
+pause
+
+echo ................................
+echo Seu nome e %nome% e seu e-mail %email%
